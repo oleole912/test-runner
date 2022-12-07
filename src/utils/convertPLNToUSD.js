82e6b7d5a69d180c7344a876714365f528bd234f
Aleksandra Bukalskai <bukalska.aleksandra@gmail.com>
2022-12-07 20:47:41 +0000

    Fix convertPLNToUSD function

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,5 +1,9 @@
 export const convertPLNToUSD = (PLN) => {
 
+  if((PLN === undefined) || (typeof PLN === 'string')) return NaN;
+  if (typeof PLN !== 'number') return 'Error';
+  if(PLN < 0) PLN = 0;
+
   const PLNtoUSD = PLN / 3.5;
 
   const formatter = new Intl.NumberFormat('en-US', {
@@ -7,8 +11,5 @@ export const convertPLNToUSD = (PLN) => {
     currency: 'USD'
   });
 
-  if((PLN === undefined) || (typeof PLN === 'string')) return NaN;
-  if (typeof PLN !== 'number') return 'Error';
-  if(PLN < 0) return '$0.00';
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
 }
